3be3f2c82008726f40c3998d117cd66ddc3eb625
johnlewczuk <70816460+johnlewczuk@users.noreply.github.com>
2024-05-13 15:12:07 -0400

    Update auto_ss.py
    made filename windows friendly

--- auto_ss.py
+++ auto_ss.py
@@ -8,18 +8,16 @@ lower_time_bound = datetime.time(hour=8)
 
 while True:
     lower_folder_name = str(datetime.date.today())
-    root_dir = os.path.expanduser("~/Desktop/ss")
+    root_dir = os.path.expanduser(os.path.join("~", "Desktop", "ss"))
     save_path = os.path.join(root_dir, lower_folder_name)
     
     curr_day = datetime.datetime.now()
     curr_time = curr_day.time()
+    # can't use colon in windows filename
+    png_name = os.path.join(save_path, f"{curr_time.hour}.{str(curr_time.minute).zfill(2)}.png")
     if(lower_time_bound <= curr_time <= upper_time_bound and curr_day.weekday() <= 4):
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
-        ss = pyautogui.screenshot()
-        ss.save(os.path.join(save_path, f"{curr_time.hour}:{str(curr_time.minute).zfill(2)}.png"))
+        pyautogui.screenshot(png_name)
         time.sleep(60)
-        
-
-
