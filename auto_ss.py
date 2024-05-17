9621923bc6becdefcb05d538b06cccc338cf378b
johnlewczuk <70816460+johnlewczuk@users.noreply.github.com>
2024-05-17 10:47:47 -0400

    Update auto_ss.py
    added ability to specify save directory

--- auto_ss.py
+++ auto_ss.py
@@ -2,22 +2,40 @@ import pyautogui
 import os
 import datetime
 import time
+import argparse
 
-upper_time_bound = datetime.time(hour=19)
-lower_time_bound = datetime.time(hour=8)
+if __name__ == '__main__':
 
-while True:
-    lower_folder_name = str(datetime.date.today())
-    root_dir = os.path.expanduser(os.path.join("~", "Desktop", "ss"))
-    save_path = os.path.join(root_dir, lower_folder_name)
+
+    parser = argparse.ArgumentParser()
+ 
+    # Adding optional argument
+    parser.add_argument("-d", "--dir", help = "Directory screenshots are saved to. Default is ..../Desktop/ss")
     
-    curr_day = datetime.datetime.now()
-    curr_time = curr_day.time()
-    # can't use colon in windows filename
-    png_name = os.path.join(save_path, f"{curr_time.hour}.{str(curr_time.minute).zfill(2)}.png")
-    if(lower_time_bound <= curr_time <= upper_time_bound and curr_day.weekday() <= 4):
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
+    # Read arguments from command line
+    args = parser.parse_args()
+    root_dir = os.path.expanduser(os.path.join("~", "Desktop", "ss"))
+ 
+    if args.dir:
+        root_dir = args.dir
 
-        pyautogui.screenshot(png_name)
-        time.sleep(60)
+    upper_time_bound = datetime.time(hour=19)
+    lower_time_bound = datetime.time(hour=8)
+
+    while True:
+        lower_folder_name = str(datetime.date.today())
+        save_path = os.path.join(root_dir, lower_folder_name)
+        
+        curr_day = datetime.datetime.now()
+        curr_time = curr_day.time()
+        # can't use colon in windows filename
+        png_name = os.path.join(save_path, f"{curr_time.hour}.{str(curr_time.minute).zfill(2)}.png")
+        if(lower_time_bound <= curr_time <= upper_time_bound and curr_day.weekday() <= 4):
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+
+            pyautogui.screenshot(png_name)
+            time.sleep(60)
+            
+
+
